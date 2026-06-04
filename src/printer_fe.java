679448afd370a551d3cad3202a845d971edef62c
Douglas Miller <durgadas311@gmail.com>
2026-06-04 07:35:02 -0500

    fixup prop handling in basic widget

--- src/printer_fe.java
+++ src/printer_fe.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Vector;
 import java.util.Properties;
 import java.awt.*;
+import java.io.*;
 
 public class printer_fe {
 	private static Diablo630 front_end;
@@ -13,18 +14,33 @@ public class printer_fe {
 	}
 	public void _main(String[] args) {
 		// TODO: make font (cpi/lpi) configurable
+		Properties props = new Properties();
+		String conf = System.getProperty("user.home") + "/.diablo630rc";
+		for (String arg : args) {
+			if (arg.startsWith("conf=")) {
+				conf = arg.substring(5);
+			}
+		}
+		try {
+			InputStream is = new FileInputStream(conf);
+			props.load(is);
+		} catch (Exception ee) {
+			//ee.printStackTrace();
+			System.err.format("No config file \"%s\"\n", conf);
+		}
+
 		Vector<String> argv = new Vector<String>(Arrays.asList(args));
 		// These are the defaults already...
 		//argv.add("cpi=10");
 		//argv.add("lpi=6");
 		//argv.add("font=Monospaced,PLAIN,12");
-		front_end = new Diablo630(new Properties(), argv, System.in);
+		front_end = new Diablo630(props, argv, System.in);
 		String file = "out.ps";
 		for (String arg : args) {
-			if (arg.indexOf("=") > 0) continue;
-			if (arg.equals("nogui")) continue;
-			file = arg;
-			break;
+			if (arg.startsWith("file=")) {
+				file = arg.substring(5);
+				break;
+			}
 		}
 		front_end.runPrinter(file);
 		// If this returns, we are really done...

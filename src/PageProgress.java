2bfa881255a7b42609eea60df70db8f8ff064269
Douglas Miller <durgadas311@gmail.com>
2026-06-12 19:29:37 -0500

    make progress thumbnail show prev as well as current page

--- src/PageProgress.java
+++ src/PageProgress.java
@@ -9,19 +9,22 @@ class PageProgress extends JPanel
 {
 	int pw;
 	int ph;
-	BufferedImage img;
+	int cur = 0;
+	BufferedImage[] img;
 	int black = Color.black.getRGB();
 	int v;
 
 	public PageProgress() {
 		super();
+		img = new BufferedImage[2];
 		setPreferredSize(new Dimension(120, 90));
 		setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
 	}
 
 	public void newPage() {
-		if (img == null) return;
-		Graphics2D g2d = img.createGraphics();
+		cur ^= 1;
+		if (img[cur] == null) return;
+		Graphics2D g2d = img[cur].createGraphics();
 		g2d.setColor(Color.white);
 		g2d.fillRect(0, 0, pw, ph);
 		v = 0;
@@ -29,12 +32,12 @@ class PageProgress extends JPanel
 	}
 
 	public void setPrint(int x, int y, int len) {
-		if (img == null) return;
+		if (img[cur] == null) return;
 		int xx = (int)Math.round((float)x / 10f);
 		int yy = (int)Math.round((float)y / 10f);
 		v = yy;
 		for (int a = 0; a < len; ++a) {
-			img.setRGB(xx + a, yy, black);
+			img[cur].setRGB(xx + a, yy, black);
 		}
 		repaint();
 	}
@@ -42,16 +45,20 @@ class PageProgress extends JPanel
 	public void setPage(int pw, int ph) {
 		this.pw = (int)Math.round((float)pw / 10f);
 		this.ph = (int)Math.round((float)ph / 10f);
-		img = new BufferedImage(this.pw, this.ph, BufferedImage.TYPE_INT_ARGB);
+		img[0] = new BufferedImage(this.pw, this.ph, BufferedImage.TYPE_INT_ARGB);
+		img[1] = new BufferedImage(this.pw, this.ph, BufferedImage.TYPE_INT_ARGB);
 		newPage();
 		repaint();
 	}
 
-	public void paint(Graphics g) {
-		super.paint(g);
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D)g;
-		if (img != null) {
-			g2d.drawImage(img, null, 5, 80 - v);
+		if (img[cur] != null) {
+			g2d.drawImage(img[cur], null, 5, 80 - v);
+		}
+		if (img[cur ^ 1] != null) {
+			g2d.drawImage(img[cur ^ 1], null, 5, 80 - v - ph - 3);
 		}
 	}
 }
